4b8d7ca17e326587e43e3985ec7637f99a96290b
elgynart <elgynart@gmail.com>
2022-04-24 02:38:26 +0300

    localhost

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 const fs = require('fs');
 const childp = require('child_process');
 
-const host = '0.0.0.0';
+const host = 'localhost';
 const port = process.env.PORT || 3000;
 
 let start = 'start';
